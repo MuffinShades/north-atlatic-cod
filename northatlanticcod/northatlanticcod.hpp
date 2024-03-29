1119d50ba2ef7f5330da209bcdef1da11d45869e
MuffinShades <89941934+MuffinShades@users.noreply.github.com>
2024-03-29 10:11:03 -0400

    main header file pog

---
+++ northatlanticcod/northatlanticcod.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ *
+ * Base header file for the library
+ * 
+ * Make sure to include this file in every header so you can
+ * add the classes to the namespace
+ * 
+ */
+
+namespace NorthAtlanticCod {};
